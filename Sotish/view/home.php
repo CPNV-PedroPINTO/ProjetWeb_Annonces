8a95e19f1ad0609d7cbbf66f0dc917b54202805c
LAVAUD Mahe <Mahe.LAVAUD@cpnv.ch>
2021-02-08 09:19:38 +0100

    df
    df

--- Sotish/view/home.php
+++ Sotish/view/home.php
@@ -10,71 +10,6 @@
 ob_start();
 $title = "SnowPoint . Accueil";
 ?>
-    <!-- Slide1 -->
- <!--   <section class="slide1">
-        <div class="wrap-slick1">
-            <div class="slick1">
-                <div class="item-slick1 item1-slick1" style="background-image: url(view/content/images/home_slide_men.jpg);">
-                    <div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
-						<span class="caption1-slide1 m-text1 t-center animated visible-false m-b-15" data-appear="fadeInDown">
-							Collection Hommes 2020
-						</span>
-
-                        <h2 class="caption2-slide1 xl-text1 t-center animated visible-false m-b-37" data-appear="fadeInUp">
-                            Nos nouveautés
-                        </h2>
-
-                        <div class="wrap-btn-slide1 w-size1 animated visible-false" data-appear="zoomIn">
-                             Button
-                            <a href="index.php?action=displayArticles" class="flex-c-m size2 bo-rad-23 s-text2 bgwhite hov1 trans-0-4">
-                                En savoir plus
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="item-slick1 item2-slick1" style="background-image: url(view/content/images/home_slide_women3.jpg);">
-                    <div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
-						<span class="caption1-slide1 m-text1 t-center animated visible-false m-b-15" data-appear="rollIn">
-							Collection Femmes 2020
-						</span>
-
-                        <h2 class="caption2-slide1 xl-text1 t-center animated visible-false m-b-37" data-appear="lightSpeedIn">
-                            New arrivals
-                        </h2>
-
-                        <div class="wrap-btn-slide1 w-size1 animated visible-false" data-appear="slideInUp">
-                             Button
-                            <a href="index.php?action=displayArticles" class="flex-c-m size2 bo-rad-23 s-text2 bgwhite hov1 trans-0-4">
-                                Shop Now
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="item-slick1 item3-slick1" style="background-image: url(view/content/images/home_slide_child2.jpg);">
-                    <div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
-						<span class="caption1-slide1 m-text1 t-center animated visible-false m-b-15" data-appear="rotateInDownLeft">
-							Collection Enfants 2020
-						</span>
-
-                        <h2 class="caption2-slide1 xl-text1 t-center animated visible-false m-b-37" data-appear="rotateInUpRight">
-                            Nos nouveautés
-                        </h2>
-
-                        <div class="wrap-btn-slide1 w-size1 animated visible-false" data-appear="rotateIn">
-                             Button
-                            <a href="index.php?action=displayArticles" class="flex-c-m size2 bo-rad-23 s-text2 bgwhite hov1 trans-0-4">
-                                En savoir plus
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-   </section> -->
-
     <!-- Banner -->
     <section class="banner bgwhite p-t-40 p-b-40">
         <div class="container">
@@ -141,28 +76,6 @@ $title = "SnowPoint . Accueil";
                             <a href="#" class="flex-c-m size2 m-text2 bg3 hov1 trans-0-4">
                                 Nidecker
                             </a>
-                        </div>
-                    </div>
-
-                    <!-- block2 -->
-                    <div class="block2 wrap-pic-w pos-relative m-b-30">
-                        <img src="view/content/images/icons/bg-01.jpg" alt="IMG">
-
-                        <div class="block2-content sizefull ab-t-l flex-col-c-m">
-                            <h4 class="m-text4 t-center w-size3 p-b-8">
-                                Inscription & 20% de rabais
-                            </h4>
-
-                            <p class="t-center w-size4">
-                                Soyez le premier à être au courant des dernières nouveautés
-                            </p>
-
-                            <div class="w-size2 p-t-25">
-                                <!-- Button -->
-                                <a href="#" class="flex-c-m size2 bg4 bo-rad-23 hov1 m-text3 trans-0-4">
-                                    Sign Up
-                                </a>
-                            </div>
                         </div>
                     </div>
                 </div>
